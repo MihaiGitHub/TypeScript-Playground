5abc45753a531a7fb635f936da457733ff916a09
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-11-09 20:11:44 -0700

    Objects

---
+++ objects.ts
@@ -0,0 +1,19 @@
+const profile = {
+  name: "alex",
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15,
+  },
+  setAge(age: number): void {
+    this.age = age;
+  },
+};
+
+// destructuring age from profile; have to say what age is inside profile when doing that
+const { age }: { age: number } = profile;
+
+// look at coords property and pull out lat, lng from the profile object : then describe the structure of the profile object
+const {
+  coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;

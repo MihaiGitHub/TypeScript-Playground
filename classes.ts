e8239699d2492d6d4f03b9856a848a1dd6f79c7c
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-04 18:50:44 -0700

    Class constructors

--- classes.ts
+++ classes.ts
@@ -1,4 +1,6 @@
 class Vehicle {
+  constructor(public color: string) {}
+
   // by default these methods are public, but can also put public in front
   drive(): void {
     console.log("Chugga chugga");
@@ -10,6 +12,12 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
+  // no public modifier for color here because it is in parent class
+  constructor(public wheels: number, color: string) {
+    // reference to the parent constructor and must pass parent required arguments
+    super(color);
+  }
+
   // if want method to be diferent than parent class, just redefine in child class
   // but can not change the type of the parent
   private drive2(): void {
@@ -22,5 +30,8 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
+const car = new Car(4, "Red");
 car.startDriving();
+
+const vehicle = new Vehicle("Orange");
+console.log(vehicle.color);
